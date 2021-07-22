ffd5d7ea1fa238895b2c83fe4fe885b4b3c824d9
Yan Santana <yan.barreiro@cubos.io>
2021-07-21 23:44:32 -0400

    feat: first use of framer motion and tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,36 @@
+import {motion} from "framer-motion";
+import Link from "next/link";
+
+interface CardProps{
+href: string;
+imgSrc: string;
+layoutId: string;
+}
+
+const Card: React.FC<CardProps> = ({layoutId, href,imgSrc}) => {
+  return(
+    <Link href={href}>
+      <div className="relative h-20 md:h-40 bg-gray-200 rounded-xl cursor-pointer hover:bg-gray-50 transition">
+        <motion.img src={imgSrc} className="w-full h-full" layoutId={layoutId}/>
+      </div>
+    </Link>
+  )
+}
+
 export default function Home() {
-  return <h1>Guilherme Rodz</h1>
+  return (
+  <div>
+    <motion.h1         
+    initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        transition={{delay: 1, duration: 1}} 
+        className="text-4xl sm:text-6xl lg:text-7xl mt-10 md:mt-24 leading-none tracking-tight font-extrabold text-gray-900">Construa sites modernos com agilidade sem precisar sair do HTML</motion.h1>
+    
+    <div className="grid w-full mt-10 md:mt-24 grid-cols-1 grid-rows-3 gap-4 md:grid-cols-3 md:grid-rows-1">
+      <Card href="/nextjs" imgSrc="/img/nextjs.svg" layoutId="nextjs-logo"/>
+      <Card href="/tailwind" imgSrc="/img/tailwind.svg" layoutId="tailwind-logo"/>
+      <Card href="/framermotion" imgSrc="/img/framermotion.svg" layoutId="framermotion-logo"/>
+    </div>
+  </div>
+  )
 }
